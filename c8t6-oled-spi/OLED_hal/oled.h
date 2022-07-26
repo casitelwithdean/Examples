28ea1ed19f2a986ea5d8961ec771e3426feab343
casitelwithdean <995478825@qq.com>
2022-07-26 17:42:39 +0800

    1

---
+++ c8t6-oled-spi/OLED_hal/oled.h
@@ -0,0 +1,82 @@
+#ifndef __OLED_H
+#define __OLED_H			  	 
+#include <stdint.h>
+#include "stdlib.h"	 
+#include <stm32f10x.h>   
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK NANO STM32开发板
+//SSD1306 OLED 驱动IC驱动代码
+//驱动方式:8080并口/4线串口
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2018/07/18
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2018-2028
+//All rights reserved	  
+//********************************************************************************  
+ //IO口操作宏定义
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
+//IO口地址映射
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C 
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C 
+#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C 
+#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C 
+#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C 
+#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C    
+#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C    
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808 
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08 
+#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008 
+#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408 
+#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808 
+#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08 
+#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08 
+//OLED模式设置
+//0: 4线串行模式  （模块的BS1，BS2均接GND）
+//1: 并行8080模式 （模块的BS1，BS2均接VCC）
+#define OLED_MODE 0
+		    	#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出 
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入 					  
+//-----------------OLED端口定义----------------  
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出 
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入 
+
+#define OLED_CS   PBout(3)
+#define OLED_RST  PBout(10)
+#define OLED_RS   PBout(11)	  
+#define OLED_RD   PBout(9)
+#define OLED_WR   PCout(13)	
+
+//PA0~7,作为数据线
+#define DATAOUT(x) GPIOA->ODR=(GPIOA->ODR&0xff00)|(x&0x00FF); //输出
+  
+//使用4线串行接口时使用 
+#define OLED_SCLK PAout(1)
+#define OLED_SDIN PAout(2)
+		     
+#define OLED_CMD  0	//写命令
+#define OLED_DATA 1	//写数据
+//OLED控制用函数
+void OLED_WR_Byte(uint8_t dat,uint8_t cmd);	    
+void OLED_Display_On(void);
+void OLED_Display_Off(void);
+void OLED_Refresh_Gram(void);		   
+							   		    
+void OLED_Init(void);
+void OLED_Clear(void);
+void OLED_DrawPoint(uint8_t x,uint8_t y,uint8_t t);
+void OLED_Fill(uint8_t x1,uint8_t y1,uint8_t x2,uint8_t y2,uint8_t dot);
+void OLED_ShowChar(uint8_t x,uint8_t y,uint8_t chr,uint8_t size,uint8_t mode);
+void OLED_ShowNum(uint8_t x,uint8_t y,uint32_t num,uint8_t len,uint8_t size);
+void OLED_ShowString(uint8_t x,uint8_t y,const uint8_t *p,uint8_t size);	 
+#endif  
+	 
+
+
+
